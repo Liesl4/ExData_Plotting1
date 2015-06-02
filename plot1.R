ebdb659a82388136b191aed0adbcfb5e208395c1
LieslS <LieslS@users.noreply.github.com>
2015-06-02 09:19:09 -0600

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,20 @@
+## Function to create the first plot for Coursera Exploratory Data Analysis project 1.
+
+plot_1 <- function( ){
+      ## Read the data set
+      power_con <- read.csv( "household_power_consumption.txt", sep = ";", skip = 66636, nrows = 2880,
+                             col.names = c( "Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage",
+                                            "Global_intensity", "sub_metering_1", "sub_metering_2", "sub_metering_3" ),
+                             na.strings = "?"
+                                 )
+      
+      ## Plot 1
+      ## Histogram of Global Active Power
+      ## Prints to png file
+      png( "Plot1.png" )
+      hist( power_con$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red" )
+      dev.off()
+      
+      ## Return extracted data
+#       power_con
+}
